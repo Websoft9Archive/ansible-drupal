5559cf485d56d9337d3e1bbc290982eecb654d31
hliblack <hliblack@outlook.com>
2019-10-28 16:39:31 +0800

    v1.2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ansible-drupal
+## Drupal
+drupal8.x 版本以上，安装完后出提示一个错误 *Protecting against HTTP HOST Header attacks* 错误，解决方法如下：
+1. 通过 WinSCP 远程连接上服务器，进入 */data/wwwroot/drupal/sites/default* 目录，将 settings.php 文件下载到本地；
+2. 打开文件，找到如下图所示的配置段：
+   ![](http://libs.websoft9.com/Websoft9/DocsPicture/zh/drupal/drupal-settings-1-websoft9.jpg)
+3. 修改为如下图所示的配置（域名请根据实际情况更改，注意红框标注的注释符）：
+   ![](http://libs.websoft9.com/Websoft9/DocsPicture/zh/drupal/drupal-settings-2-websoft9.jpg)
+4. 重新上传至原目录下；
+5. 执行 *chown -R apache: /data/wwwroot/drupal* 命令，刷新页面，错误提示消失。
--- roles/drupal/files/init.sh
+++ roles/drupal/files/init.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-old_password=$(cat /credentials/password.txt | awk 'NR==2' |awk -F ":" '{print $2}' )
+old_password=$(cat /credentials/password.txt | awk 'NR==2' |awk -F ":" '{print $2}')
+
 new_root_password=$(pwgen -Acns 8 1)
 new_drupal_password=$(pwgen -Acns 8 1)
 
 systemctl restart mysql
 
-hosts=$(echo $(mysql -uroot -p${old_password} -e "select host from mysql.user where user='root';") | sed 's/^host//')
+root_hosts=$(echo $(mysql -uroot -p${old_password} -e "select host from mysql.user where user='root';") | sed 's/^host//')
+drupal_hosts=$(echo $(mysql -umoodle -p${old_password} -e "select host from mysql.user where user='moodle';") | sed 's/^host//')
 
-for i in $hosts
+for i in $root_hosts
 do
   mysqladmin -uroot -p${old_password} -h $i password $new_root_password
 done
 
-mysqladmin -udrupal -p${old_password} -h localhost password $new_drupal_password
+for j in $drupal_hosts
+do
+  mysqladmin -umoodle -p${old_password} -h $j password $new_drupal_password
+done
 
-sed -i "s/$old_password/$new_password/" /credentials/password.txt
-
-echo -e "/nMySQL username:drupal\nMySQL Password:"$new_drupal_password >> /credentials/password.txt
+echo -e 'MySQL username:root\nMySQL Password:'$new_root_password  > /credentials/password.txt
+echo -e '\nMySQL username:drupal\nMySQL Password:'$new_drupal_password  >> /credentials/password.txt
 
 sed -i 's/\/root\/init.sh//g' /etc/rc.local
 
 rm -rf /root/init.sh
-
